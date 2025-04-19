17e9bfbe49a1b7c4315a6df79bd357b04306a7d2
isamu <isamu@to-kyo.to>
2025-04-19 12:54:55 +0900

    update test

--- test/test_string.ts
+++ test/test_string.ts
@@ -17,13 +17,24 @@ const scriptData = {
   imageIndex: 1,
 };
 
+const scriptData2 = {
+  speaker: "",
+  text: "あかさかさかす。スカイツリーかな！ハウアーユーってなに？",
+  caption: "",
+  instructions: "",
+  duration: 2,
+  filename: "",
+  imagePrompt: "",
+  imageIndex: 2,
+};
+
 test("test splitIntoSentences", async () => {
   const ret = splitIntoSentences(text, "。", 7);
   assert.deepStrictEqual(ret, ["あかさたなはまやらわ。", "東京特許許可局！今日はいい天気ですか？"]);
 });
 
 test("test recursiveSplit", async () => {
-  const ret = recursiveSplit([scriptData]);
+  const ret = recursiveSplit([scriptData, scriptData2]);
   const expect = [
     {
       speaker: "",
@@ -54,6 +65,36 @@ test("test recursiveSplit", async () => {
       filename: "",
       imagePrompt: "",
       imageIndex: 1,
+    },
+    {
+      speaker: "",
+      text: "あかさかさかす。",
+      caption: "",
+      instructions: "",
+      duration: 2,
+      filename: "",
+      imagePrompt: "",
+      imageIndex: 2,
+    },
+    {
+      speaker: "",
+      text: "スカイツリーかな！",
+      caption: "",
+      instructions: "",
+      duration: 2,
+      filename: "",
+      imagePrompt: "",
+      imageIndex: 2,
+    },
+    {
+      speaker: "",
+      text: "ハウアーユーってなに？",
+      caption: "",
+      instructions: "",
+      duration: 2,
+      filename: "",
+      imagePrompt: "",
+      imageIndex: 2,
     },
   ];
   assert.deepStrictEqual(ret, expect);
