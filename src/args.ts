fdc6d825b406a295ec113892208e4e9c0e38c890
isamu <isamu@to-kyo.to>
2025-04-20 11:49:47 +0900

    add args

---
+++ src/args.ts
@@ -0,0 +1,28 @@
+import yargs from "yargs";
+
+
+export const args = yargs
+  .scriptName("mulmocast")
+  .option("outdir", {
+    description: "output dir",
+    demandOption: false,
+    type: "string",
+  })
+  .option("scratchpad", {
+    description: "scratchpad dir",
+    demandOption: false,
+    type: "string",
+  })
+  .option("basedir", {
+    description: "base dir",
+    demandOption: false,
+    type: "string",
+  })
+  .command("* <ScriptFile>", "run mulmocast.")
+/*  .positional("CCC", {
+    describe: "DDD",
+    type: "string",
+    demandOption: false,
+  })*/
+  .parseSync();
+
