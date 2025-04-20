bf1a2ef54f99f4fc9861893e32ea05e5542fe4b1
snakajima <satoshi.nakajima@gmail.com>
2025-04-19 15:36:04 -1000

    generate hawaii.png

--- src/proto/image.ts
+++ src/proto/image.ts
@@ -1,31 +1,42 @@
 import "dotenv/config";
-import { GraphAI, AgentFilterFunction, GraphData } from "graphai";
+import { GraphAI, GraphData } from "graphai";
 import * as agents from "@graphai/agents";
 import imageOpenaiAgent from "../agents/image_openai_agent";
+import { fileWriteAgent } from "@graphai/vanilla_node_agents";
+
 
 const graph_data: GraphData = {
   version: 0.5,
   nodes: {
-		image: {
+		imageGenerator: {
 			agent: "imageOpenaiAgent",
 			inputs: {
 				prompt: "Beatiful sunset in Hawaii"
 			},
+		},
+    fileWriter: {
+      agent: "fileWriteAgent",
+      inputs: {
+        file: "./output/hawaii.png",
+				buffer: ":imageGenerator.buffer"
+      },
+      params: { baseDir: __dirname + "/../../" },
 			isResult: true
-		}
+    }
 	}
 };
 
-const test = async () => {
+const main = async () => {
 	const graph = new GraphAI(
 		graph_data,
 		{
 			...agents,
 			imageOpenaiAgent,
+			fileWriteAgent
 		}
 	);
 	const results = await graph.run();
 	console.log(results);
 }
 
-test();
+main();
