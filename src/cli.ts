4b83831de6d0e6628433c7d4293ae70562f1bcf8
isamu <isamu@to-kyo.to>
2025-04-20 11:44:50 +0900

    add yargs

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,13 @@
+#!/usr/bin/env node
+
+import "dotenv/config";
+
+import { args } from "./args";
+
 const main = () => {
+  if (args.log) {
+    console.log("log")
+  }
   console.log("hello");
 };
 main();
