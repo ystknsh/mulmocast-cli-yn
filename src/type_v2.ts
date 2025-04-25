b910f674dcc942e802fe7ebef8b59e38d664d33e
isamu <isamu@to-kyo.to>
2025-04-25 11:41:09 +0900

    type v2

---
+++ src/type_v2.ts
@@ -0,0 +1,70 @@
+
+type SPEAKER = string;
+type LANG = string;
+
+type originalText = {
+  text: string;
+  lang: LANG;
+};
+
+type LocalizedText = {
+  text: string;
+  captions: string;
+  ttsTexts: string;
+};
+
+// Page Data
+export type ScriptData = {
+  speaker: SPEAKER;
+  
+  text: OriginalText;
+  multiLingualText: Record<LANG, LocalizedText>
+  
+  // ttsText: string | undefined;
+  instructions: string | undefined;  // tts_options for open ai
+  duration: number; // generated // video duration tine (ms)
+  filename: string; // generated // 
+  imagePrompt: string | undefined; // inserted by LLM
+};
+
+export type ImageInfo = {
+  index: number;
+  imagePrompt: string | undefined;
+  image: string | undefined; // path to the image
+};
+
+export type VoiceMap = Record<string, string>;
+
+// epsode
+export type MulmoScript = {
+  // global setting
+  title: string;
+  description: string;
+  reference: string;
+
+  // mulmoScript PageData
+  // script: ScriptData[];
+  pages: ScriptData[];
+
+  // for tts
+  tts: string | undefined; // default: openAI
+  speakers: SPEAKER[] | undefined;
+  voicemap: VoiceMap; // generated
+
+  // for graph data
+  voices: string[] | undefined;
+  ttsAgent: string; // generated
+
+  filename: string; // generated 
+  // imageInfo: any[]; // generated
+
+  // for video and image
+  aspectRatio: string | undefined; // "16:9" or "9:16" for movie and images
+  images: ImageInfo[]; // generated
+  imagePath: string | undefined; // for Keynote images movie ??
+  omitCaptions: boolean | undefined; // default is false
+
+  // for bgm
+  padding: number | undefined; // for bgm
+
+};
