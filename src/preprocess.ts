b7fe064d7bcfbb056c3a3d59a9891cb5c2bf5c2b
isamu <isamu@to-kyo.to>
2025-04-27 04:25:15 +0900

    fix mulmo generator

--- src/preprocess.ts
+++ src/preprocess.ts
@@ -19,7 +19,7 @@ const granslateGraph: GraphData = {
       isResult: true,
       agent: "mergeObjectAgent",
       inputs: {
-        items: [":mulmoScript", { fileName: ":fileName" }, ":beatsMap.mergeResult"],
+        items: [":mulmoScript", { fileName: ":fileName" }, { beats: ":beatsMap.mergeBeatData"}],
       },
     },
     beatsMap: {
@@ -130,29 +130,23 @@ const granslateGraph: GraphData = {
             },
           },
           */
-          mergeBeat: {
-            agent: (namedInputs: { localizedTexts: LocalizedText[]; beat: MulmoBeat }) => {
-              const { localizedTexts, beat } = namedInputs;
-              const multiLingualTexts = localizedTexts.reduce((tmp: MultiLingualTexts, translateResult: LocalizedText) => {
-                const { lang } = translateResult;
-                tmp[lang] = translateResult;
-                return tmp;
-              }, {});
-              beat.multiLingualTexts = multiLingualTexts;
-              return beat;
-            },
+          mergeLocalizedText: {
+            agent: "arrayToObjectAgent",
             inputs: {
-              localizedTexts: ":preprocessBeats.ttsTexts",
-              beat: ":row",
+              items: ":preprocessBeats.ttsTexts"
+            },
+            params: {
+              key: "lang",
             },
           },
-          mergeResult: {
+          mergeBeatData: {
             isResult: true,
             agent: "mergeObjectAgent",
             inputs: {
-              items: [":mergeBeat"],
+              items: [":row", {multiLingualTexts: ":mergeLocalizedText"}],
               // imagePrompt: ":imagePrompt",
             },
+            // console: {before: true, after: true},
           },
         },
       },
