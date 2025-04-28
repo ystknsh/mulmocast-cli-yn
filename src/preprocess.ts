0a3065bd64ad73348da5ed9dc004c793f5fae11b
isamu <isamu@to-kyo.to>
2025-04-29 04:23:02 +0900

    update text cache

--- src/preprocess.ts
+++ src/preprocess.ts
@@ -202,30 +202,14 @@ const translateText = async (mulmoScript: MulmoScript, fileName: string, default
   return results.mergeResult;
 };
 
-export const updateMultiLingualTexts = (originalMulmoData: MulmoScript, mulmoData: MulmoScript, defaultLang: Lang) => {
-  const lang = originalMulmoData.lang ?? defaultLang;
+export const updateMultiLingualTexts = (originalMulmoData: MulmoScript, mulmoData: MulmoScript): MulmoScript => {
   mulmoData.beats = mulmoData.beats.map((beat, index) => {
-    // Not translated yet
-    if (!beat.multiLingualTexts) {
-      return beats;
+    const originalBeat = originalMulmoData?.beats[index];
+    if (originalBeat?.text === beat?.text) {
+      return beat;
     }
-    // No default target Language data. maybe changed langu??
-    if (!beat.multiLingualTexts[lang]) {
-      beat.multiLingualTexts = {};
-      return beats;
-    }
-    // index was changed
-    if (!originalMulmoData.beats[index]) {
-      beat.multiLingualTexts = {};
-      return beats;
-    }
-    //
-    if (beat.multiLingualTexts[lang].text !== originalMulmoData.beats[index].text) {
-      beat.multiLingualTexts = {};
-      return beats;
-    }
-    return beats;
-  });
+    return originalBeat;
+  }).filter(beat => beat);
   return mulmoData;
 };
 
@@ -240,13 +224,15 @@ const main = async () => {
   const outputFilePath = getOutputFilePath(fileName + ".json");
   const { mulmoData } = readMulmoScriptFile(outputFilePath) ?? { mulmoData: originalMulmoData };
 
-  const targetMulmoData = updateMultiLingualTexts(originalMulmoData, mulmoData, defaultLang);
+  const targetMulmoData = updateMultiLingualTexts(originalMulmoData, mulmoData);
 
-  const mulmoDataResult = await translateText(mulmoData, fileName, defaultLang, targetLangs);
+  const mulmoDataResult = await translateText(targetMulmoData, fileName, defaultLang, targetLangs);
 
   console.log(JSON.stringify(mulmoDataResult, null, 2));
 
   // console.log(mulmoData, originalMulmoData);
 };
 
-main();
+if (process.argv[1] === __filename) {
+  main();
+}
---
+++ test/v2/test_mulmo_data.ts
@@ -0,0 +1,100 @@
+import { updateMultiLingualTexts } from "../../src/preprocess";
+
+import test from "node:test";
+import assert from "node:assert";
+
+const beat = {
+  speaker: "",
+  text: "123",
+  multiLingualTexts: {},
+  instructions: "",
+  imagePrompt: "",
+  image: "",
+  filename: "",
+}
+
+const data = {
+  title: "",
+  description: "",
+  reference: "",
+  speed: 1.666,
+  aspectRatio: "9:16",
+  tts: "",
+  lang: "ja",
+  beats: [],
+  filename: "",
+  speakers: {},
+  text2image: {
+    model: "",
+    size: "",
+    aspectRatio: "",
+    style: "",
+    provider: "",
+  },
+  imagePath: "",
+  omitCaptions: true,
+  padding: 1,
+};
+test("test updateMultiLingualTexts not update", async () => {
+  const originalBeat = {
+    ...beat,
+  };
+  const originalData = {
+    ...data,
+    beats: [originalBeat]
+  };
+  
+  const updateBeat = {
+    ...beat,
+  };
+  const updateData = {
+    ...data,
+    beats: [updateBeat]
+  };
+  const ret = updateMultiLingualTexts(originalData, updateData);
+  assert.equal(ret.beats[0].text, "123");
+});
+
+test("test updateMultiLingualTexts update", async () => {
+  const originalBeat = {
+    ...beat,
+    text: "123123",
+  };
+  const originalData = {
+    ...data,
+    beats: [originalBeat]
+  };
+  
+  const updateBeat = {
+    ...beat,
+  };
+  const updateData = {
+    ...data,
+    beats: [updateBeat]
+  };
+  const ret = updateMultiLingualTexts(originalData, updateData);
+  assert.equal(ret.beats[0].text, "123123");
+});
+
+test("test updateMultiLingualTexts update", async () => {
+  const originalBeat = {
+    ...beat,
+    text: "123123",
+  };
+  const originalData = {
+    ...data,
+    beats: [originalBeat]
+  };
+  
+  const updateBeat = {
+    ...beat,
+  };
+  const updateData = {
+    ...data,
+    beats: [updateBeat, originalBeat]
+  };
+  const ret = updateMultiLingualTexts(originalData, updateData);
+  assert.equal(ret.beats.length, 1);
+  assert.equal(ret.beats[0].text, "123123");
+  // assert.equal(ret.beats[0].text, "123123");
+});
