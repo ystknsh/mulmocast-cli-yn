4f9616e78e745e40c94ebd820c165f6032abc391
snakajima <satoshi.nakajima@gmail.com>
2025-04-30 14:33:09 -0700

    default styles

--- src/images.ts
+++ src/images.ts
@@ -20,15 +20,25 @@ dotenv.config();
 import { GoogleAuth } from "google-auth-library";
 
 async function convertMarkdownToImage(markdown: string, outputPath: string) {
+  // Step 0: Prepare the header
+  const styles = [
+    "body { margin: 40px; margin-top: 60px; color:#333 }",
+    "h1 { font-size: 60px; text-align: center }",
+    "ul { margin-left: 40px } ",
+    "li { font-size: 48px }",
+  ];
+  const header = `<head><style>${styles.join('\n')}</style></head>`;
+
   // Step 1: Convert Markdown to HTML
-  const html = await marked(markdown);
+  const body = await marked(markdown);
+  const html = `<htlm>${header}<body>${body}</body></html>`;
 
   // Step 2: Use Puppeteer to render HTML to an image
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   // Set the page content to the HTML generated from the Markdown
-  await page.setContent(`<htlm><head><style>body: { font-size: 80px }</style></head><body>${html}<body></html>`);
+  await page.setContent(html);
 
   // Adjust page settings if needed (like width, height, etc.)
   await page.setViewport({ width: 1200, height: 800 });
