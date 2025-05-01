1890826a32e1e55cbfe29f93ea3a677b117430df
snakajima <satoshi.nakajima@gmail.com>
2025-05-01 15:39:35 -0700

    back to main

--- lib/methods/mulmo_script.d.ts
+++ lib/methods/mulmo_script.d.ts
@@ -1,6 +1,5 @@
-import { MulmoDimension, MulmoScript } from "../types";
+import { MulmoScript } from "../types";
 export declare const MulmoScriptMethods: {
     getPadding(script: MulmoScript): number;
-    getCanvasSize(script: MulmoScript): MulmoDimension;
-    getAspectRatio(script: MulmoScript): string;
+    getSpeechProvider(script: MulmoScript): string;
 };
