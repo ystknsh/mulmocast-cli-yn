e863fff84dfe26f64a80aebb2d934be3742253b5
isamu <isamu@to-kyo.to>
2025-05-02 18:32:15 +0900

    more tests

--- test/utils/test_file_path.ts
+++ test/utils/test_file_path.ts
@@ -4,23 +4,54 @@ import test from "node:test";
 import assert from "node:assert";
 
 test("test getBaseDirPath pwd", async () => {
-  const path = getBaseDirPath()
+  const path = getBaseDirPath();
   assert.deepStrictEqual(path, process.cwd());
 });
 
 test("test getBaseDirPath absolute", async () => {
-  const path = getBaseDirPath("/foo//aa")
+  const path = getBaseDirPath("/foo//aa");
   assert.deepStrictEqual(path, "/foo/aa");
 });
 
 test("test getBaseDirPath related", async () => {
-  const path = getBaseDirPath("foo//aa")
+  const path = getBaseDirPath("foo//aa");
   assert.deepStrictEqual(path, process.cwd() + "/foo/aa");
 });
 
+// arg1 = related(foo), full(/foo//aa), undefined,  / arg2 = related(bar) full(//bar/bb)
 
-test("test getFullPath base is not set", async () => {
-  const path = getFullPath(undefined as unknown as string, "foo//aa")
-  assert.deepStrictEqual(path, process.cwd() + "/foo/aa");
+test("test getFullPath 1", async () => {
+  // join path based on cwd
+  const path = getFullPath("foo", "bar");
+  assert.deepStrictEqual(path, process.cwd() + "/foo/bar");
 });
 
+test("test getFullPath 2", async () => {
+  // arg2
+  const path = getFullPath("foo", "//bar/bb");
+  assert.deepStrictEqual(path, "/bar/bb");
+});
+
+test("test getFullPath 3", async () => {
+  // join path
+  const path = getFullPath("/foo//aa", "bar");
+  assert.deepStrictEqual(path, "/foo/aa/bar");
+});
+
+test("test getFullPath 4", async () => {
+  // arg2
+  const path = getFullPath("/foo//aa", "//bar/bb");
+  assert.deepStrictEqual(path, "/bar/bb");
+});
+
+test("test getFullPath 5", async () => {
+  // arg2 based on cwd
+  const path = getFullPath(undefined as unknown as string, "bar");
+  assert.deepStrictEqual(path, process.cwd() + "/bar");
+});
+
+test("test getFullPath 6", async () => {
+  // arg2
+  const path = getFullPath(undefined as unknown as string, "//bar/bb");
+  assert.deepStrictEqual(path, "/bar/bb");
+});
