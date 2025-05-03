73edf7828073b7dba4815191627354bc9b8180b9
isamu <isamu@to-kyo.to>
2025-05-03 19:17:54 +0900

    Common yarg settings

---
+++ src/cli/common.ts
@@ -0,0 +1,24 @@
+import type { Argv } from "yargs";
+
+export const commonOptions = (yargs: Argv) => {
+  return yargs
+    .option("v", {
+      alias: "verbose",
+      describe: "verbose log",
+      demandOption: true,
+      default: false,
+      type: "boolean",
+    })
+    .option("o", {
+      alias: "outdir",
+      description: "output dir",
+      demandOption: false,
+      type: "string",
+    })
+    .option("b", {
+      alias: "basedir",
+      description: "base dir",
+      demandOption: false,
+      type: "string",
+    });
+};
