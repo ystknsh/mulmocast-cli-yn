e8977ee040c2e75300efba8cd93a0de9efbd3d1b
snakajima <satoshi.nakajima@gmail.com>
2025-05-05 06:16:47 -0700

    format

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -138,11 +138,10 @@ export const mulmoScriptSchema = z.object({
   beats: z.array(mulmoBeatSchema),
 
   // for text2speech
-  speechParams: text2speechParamsSchema
-    .extend({
-      provider: z.string().optional(),
-      speakers: speakerDictionarySchema,
-    }),
+  speechParams: text2speechParamsSchema.extend({
+    provider: z.string().optional(),
+    speakers: speakerDictionarySchema,
+  }),
 
   // for text2image
   imageParams: text2imageParamsSchema
