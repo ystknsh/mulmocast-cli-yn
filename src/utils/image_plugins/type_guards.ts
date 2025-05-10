f32c2b5d17e015583d10440d5954b758c1c66f28
isamu <isamu@to-kyo.to>
2025-05-11 07:49:00 +0900

    more type guards

--- src/utils/image_plugins/type_guards.ts
+++ src/utils/image_plugins/type_guards.ts
@@ -1,4 +1,35 @@
-import { MulmoMermaidMedia, MulmoMermaidMediaSchema } from "../../types/index.js";
+import {
+  MulmoTextSlideMedia,
+  MulmoTextSlideMediaSchema,
+  MulmoMarkdownMedia,
+  MulmoMarkdownMediaSchema,
+  MulmoImageMedia,
+  MulmoImageMediaSchema,
+  MulmoChartMedia,
+  MulmoChartMediaSchema,
+  MulmoMermaidMedia,
+  MulmoMermaidMediaSchema,
+} from "../../types/index.js";
+
+export const isMulmoImageTextSlide = (value: unknown): value is MulmoTextSlideMedia => {
+  const result = MulmoTextSlideMediaSchema.safeParse(value);
+  return result.success;
+};
+
+export const isMulmoImageMarkdown = (value: unknown): value is MulmoMarkdownMedia => {
+  const result = MulmoMarkdownMediaSchema.safeParse(value);
+  return result.success;
+};
+
+export const isMulmoImageImage = (value: unknown): value is MulmoImageMedia => {
+  const result = MulmoImageMediaSchema.safeParse(value);
+  return result.success;
+};
+
+export const isMulmoImageChart = (value: unknown): value is MulmoChartMedia => {
+  const result = MulmoChartMediaSchema.safeParse(value);
+  return result.success;
+};
 
 export const isMulmoImageMermaild = (value: unknown): value is MulmoMermaidMedia => {
   const result = MulmoMermaidMediaSchema.safeParse(value);
