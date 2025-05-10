65df7b0f69fba071780a3f376bf750d811e6fcf8
snakajima <satoshi.nakajima@gmail.com>
2025-05-10 04:47:19 -0700

    plugin movie

--- src/utils/image_plugins/movie.ts
+++ src/utils/image_plugins/movie.ts
@@ -1,7 +1,7 @@
 import { ImageProcessorParams } from "../../types/index.js";
 import { MulmoStudioContextMethods } from "../../methods/index.js";
 
-export const imageType = "image";
+export const imageType = "movie";
 
 const processImage = (params: ImageProcessorParams) => {
   const { beat, context } = params;
