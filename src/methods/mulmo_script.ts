66e50bbfba6a0a89e2fe24d886d952a5e6606bf2
snakajima <satoshi.nakajima@gmail.com>
2025-05-12 04:20:31 -0700

    clear css

--- src/methods/mulmo_script.ts
+++ src/methods/mulmo_script.ts
@@ -3,15 +3,15 @@ import { MulmoCanvasDimension, MulmoScript, MulmoBeat, SpeechOptions, Text2Image
 import { text2ImageProviderSchema, text2SpeechProviderSchema, mulmoCanvasDimensionSchema } from "../types/schema.js";
 
 const defaultTextSlideStyles = [
-  /*'*,*::before,*::after{box-sizing:border-box}body,h1,h2,h3,h4,p,figure,blockquote,dl,dd{margin:0}ul[role="list"],ol[role="list"]{list-style:none}html:focus-within{scroll-behavior:smooth}body{min-height:100vh;text-rendering:optimizeSpeed;line-height:1.5}a:not([class]){text-decoration-skip-ink:auto}img,picture{max-width:100%;display:block}input,button,textarea,select{font:inherit}@media(prefers-reduced-motion:reduce){html:focus-within{scroll-behavior:auto}*,*::before,*::after{animation-duration:.01ms !important;animation-iteration-count:1 !important;transition-duration:.01ms !important;scroll-behavior:auto !important}}',*/
-  "body { margin: 40px; margin-top: 60px; color:#333; font-size: 48px }",
-  "h1 { font-size: 60px; text-align: center }",
+  '*,*::before,*::after{box-sizing:border-box}body,h1,h2,h3,h4,p,figure,blockquote,dl,dd{margin:0}ul[role="list"],ol[role="list"]{list-style:none}html:focus-within{scroll-behavior:smooth}body{min-height:100vh;text-rendering:optimizeSpeed;line-height:1.5}a:not([class]){text-decoration-skip-ink:auto}img,picture{max-width:100%;display:block}input,button,textarea,select{font:inherit}@media(prefers-reduced-motion:reduce){html:focus-within{scroll-behavior:auto}*,*::before,*::after{animation-duration:.01ms !important;animation-iteration-count:1 !important;transition-duration:.01ms !important;scroll-behavior:auto !important}}',
+  "body { margin: 60px; margin-top: 40px; color:#333; font-size: 30px }",
+  "h1 { font-size: 56px; margin-bottom: 20px; text-align: center }",
   "h2 { font-size: 48px }",
-  "h3 { font-size: 30px }",
+  "h3 { font-size: 36px }",
   "ul { margin-left: 40px } ",
   "pre { background: #eeeecc; font-size: 16px; padding:4px }",
   "p { margin-left: 40px }",
-  "table { font-size: 40px; margin: auto; border: 1px solid gray; border-collapse: collapse }",
+  "table { font-size: 36px; margin: auto; border: 1px solid gray; border-collapse: collapse }",
   "th { border-bottom: 1px solid gray }",
   "td, th { padding: 8px }",
   "tr:nth-child(even) { background-color: #eee }",
