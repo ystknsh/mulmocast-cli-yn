0712b89cdfa919db5629850aedb26fb162f6ff2b
snakajima <satoshi.nakajima@gmail.com>
2025-05-11 22:21:55 -0700

    hello world

--- src/methods/mulmo_script.ts
+++ src/methods/mulmo_script.ts
@@ -3,10 +3,13 @@ import { MulmoCanvasDimension, MulmoScript, MulmoBeat, SpeechOptions, Text2Image
 import { text2ImageProviderSchema, text2SpeechProviderSchema, mulmoCanvasDimensionSchema } from "../types/schema.js";
 
 const defaultTextSlideStyles = [
+  /*'*,*::before,*::after{box-sizing:border-box}body,h1,h2,h3,h4,p,figure,blockquote,dl,dd{margin:0}ul[role="list"],ol[role="list"]{list-style:none}html:focus-within{scroll-behavior:smooth}body{min-height:100vh;text-rendering:optimizeSpeed;line-height:1.5}a:not([class]){text-decoration-skip-ink:auto}img,picture{max-width:100%;display:block}input,button,textarea,select{font:inherit}@media(prefers-reduced-motion:reduce){html:focus-within{scroll-behavior:auto}*,*::before,*::after{animation-duration:.01ms !important;animation-iteration-count:1 !important;transition-duration:.01ms !important;scroll-behavior:auto !important}}',*/
   "body { margin: 40px; margin-top: 60px; color:#333; font-size: 48px }",
   "h1 { font-size: 60px; text-align: center }",
+  "h2 { font-size: 48px }",
+  "h3 { font-size: 30px }",
   "ul { margin-left: 40px } ",
-  "pre { margin-left: 40px; font-size: 32px }",
+  "pre { background: #eeeecc; font-size: 16px; padding:4px }",
   "p { margin-left: 40px }",
   "table { font-size: 40px; margin: auto; border: 1px solid gray; border-collapse: collapse }",
   "th { border-bottom: 1px solid gray }",
