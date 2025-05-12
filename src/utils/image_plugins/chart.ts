4468b88686dca7a907b0f547cd842a69b695befb
snakajima <satoshi.nakajima@gmail.com>
2025-05-12 04:41:52 -0700

    chart uses cssStyle specified

--- src/utils/image_plugins/chart.ts
+++ src/utils/image_plugins/chart.ts
@@ -5,12 +5,13 @@ import { renderHTMLToImage, interpolate } from "../markdown.js";
 export const imageType = "chart";
 
 const processChart = async (params: ImageProcessorParams) => {
-  const { beat, imagePath, canvasSize } = params;
+  const { beat, imagePath, canvasSize, textSlideStyle } = params;
   if (!beat.image || beat.image.type !== imageType) return;
 
   const template = getHTMLFile("chart");
   const htmlData = interpolate(template, {
     title: beat.image.title,
+    style: textSlideStyle,
     width: Math.round(canvasSize.width * 0.625).toString(),
     chart_data: JSON.stringify(beat.image.chartData),
   });
