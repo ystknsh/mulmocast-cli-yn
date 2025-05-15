a0d2170e20c28b5957c8c25e149d2a3de72edf1c
isamu <isamu@to-kyo.to>
2025-05-16 05:00:42 +0900

    talk support japanese

--- src/actions/pdf.ts
+++ src/actions/pdf.ts
@@ -50,10 +50,10 @@ const pdfSlide = async (pageWidth: number, pageHeight: number, imagePaths: strin
   }
 };
 
-const pdfTalk = async (pageWidth: number, pageHeight: number, imagePaths: string[], texts: string[], pdfDoc: PDFDocument) => {
+const pdfTalk = async (pageWidth: number, pageHeight: number, imagePaths: string[], texts: string[], pdfDoc: PDFDocument, font: PDFFont) => {
   const imageRatio = 0.7;
   const textMargin = 8;
-  const textY = textMargin + (pageHeight * (1 - imageRatio)) / 2;
+  const textY = pageHeight * (1 - imageRatio) - textMargin;
 
   const targetWidth = pageWidth - offset;
   const targetHeight = pageHeight * imageRatio - offset;
@@ -85,13 +85,19 @@ const pdfTalk = async (pageWidth: number, pageHeight: number, imagePaths: string
       borderColor: rgb(0, 0, 0),
       borderWidth: 1,
     });
-    page.drawText(text, {
-      x: textMargin,
-      y: textY,
-      size: 16,
-      color: rgb(0, 0, 0),
-      maxWidth: pageWidth - 2 * textMargin,
-    });
+
+    const lines = wrapText(text, font, fontSize, pageWidth - textMargin * 2);
+
+    for (const [index, line] of lines.entries()) {
+      page.drawText(line, {
+        x: textMargin,
+        y: textY - fontSize - (fontSize + 2) * index,
+        size: fontSize,
+        color: rgb(0, 0, 0),
+        width: pageWidth - 2 * textMargin,
+        font,
+      });
+    }
   }
 };
 
@@ -117,7 +123,6 @@ const pdfHandout = async (
       const originalRatio = origHeight / origWidth;
 
       const fitWidth = originalRatio / cellRatio < 1;
-      // console.log({handoutImageRatio, cellRatio, ratio,  imageHeight, origHeight});
       const pos = (() => {
         if (isLandscapeImage) {
           const cellHeight = pageHeight / imagesPerPage - offset;
@@ -153,7 +158,6 @@ const pdfHandout = async (
         borderWidth: 1,
       });
       page.drawImage(image, pos);
-      // const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
       if (isLandscapeImage) {
         const lines = wrapText(texts[index], font, fontSize, pos.width - textMargin * 2);
@@ -203,7 +207,6 @@ const pdfHandout = async (
 };
 
 const outputSize = (pdfSize: PDFSize, isLandscapeImage: boolean, isRotate: boolean) => {
-  // console.log(pdfSize);
   if (pdfSize === "a4") {
     if (isLandscapeImage !== isRotate) {
       return { width: 841.89, height: 595.28 };
@@ -244,7 +247,7 @@ export const pdf = async (context: MulmoStudioContext, pdfMode: PDFMode, pdfSize
     await pdfSlide(pageWidth, pageHeight, imagePaths, pdfDoc);
   }
   if (pdfMode === "talk") {
-    await pdfTalk(pageWidth, pageHeight, imagePaths, texts, pdfDoc);
+    await pdfTalk(pageWidth, pageHeight, imagePaths, texts, pdfDoc, customFont);
   }
 
   const pdfBytes = await pdfDoc.save();
