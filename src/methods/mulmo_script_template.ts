25e161a6a5a5c0bbfabb1e9245f6aa4961d9acbb
isamu <isamu@to-kyo.to>
2025-05-18 05:30:08 +0900

    getMulmoScriptTemplateSystemPrompt

--- src/methods/mulmo_script_template.ts
+++ src/methods/mulmo_script_template.ts
@@ -1,20 +1,8 @@
-import { zodToJsonSchema } from "zod-to-json-schema";
 import { MulmoScriptTemplate } from "../types/index.js";
-import { mulmoScriptSchema } from "../types/schema.js";
+import { getMulmoScriptTemplateSystemPrompt } from "../utils/prompt.js";
 
 export const MulmoScriptTemplateMethods = {
   getSystemPrompt(template: MulmoScriptTemplate): string {
-    // script is provided, use it as a script template
-    if (template.script) {
-      return `${template.systemPrompt}\n\`\`\`JSON\n${JSON.stringify(template.script)}\n\`\`\``;
-    }
-
-    // script is not provided, use the default schema
-    const defaultSchema = zodToJsonSchema(mulmoScriptSchema, {
-      strictUnions: true,
-    });
-
-    const specificOutputPrompt = `The output should follow the JSON schema specified below. Please provide your response as valid JSON within \`\`\`json code blocks for clarity.`;
-    return `${template.systemPrompt}\n\n${specificOutputPrompt}\n\n\`\`\`JSON\n${JSON.stringify(defaultSchema)}\n\`\`\``;
+    return getMulmoScriptTemplateSystemPrompt(template);
   },
 };
--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,4 +1,6 @@
-import { MulmoBeat } from "../types/index.js";
+import { MulmoBeat, MulmoScriptTemplate } from "../types/index.js";
+import { mulmoScriptSchema } from "../types/schema.js";
+import { zodToJsonSchema } from "zod-to-json-schema";
 
 export const imagePrompt = (beat: MulmoBeat, style?: string) => {
   return (beat.imagePrompt || `generate image appropriate for the text. text: ${beat.text}`) + "\n" + (style || "");
@@ -7,4 +9,19 @@ export const imagePrompt = (beat: MulmoBeat, style?: string) => {
 // sourceTextInput: ${:sourceText.text}
 export const graphDataScriptFromUrlPrompt = (sourceTextInput: string) => {
   return `Please create a script using the information from the following URLs as reference: ${sourceTextInput}`;
+};
+
+export const getMulmoScriptTemplateSystemPrompt = (template: MulmoScriptTemplate) => {
+  // script is provided, use it as a script template
+  if (template.script) {
+    return `${template.systemPrompt}\n\`\`\`JSON\n${JSON.stringify(template.script)}\n\`\`\``;
+  }
+
+  // script is not provided, use the default schema
+  const defaultSchema = zodToJsonSchema(mulmoScriptSchema, {
+    strictUnions: true,
+  });
+
+  const specificOutputPrompt = `The output should follow the JSON schema specified below. Please provide your response as valid JSON within \`\`\`json code blocks for clarity.`;
+  return `${template.systemPrompt}\n\n${specificOutputPrompt}\n\n\`\`\`JSON\n${JSON.stringify(defaultSchema)}\n\`\`\``;
 };
