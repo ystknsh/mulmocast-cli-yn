82a271eb5348487f14146480423188e528e65f41
snakajima <satoshi.nakajima@gmail.com>
2025-05-18 11:08:41 -0700

    graph

--- src/actions/captions.ts
+++ src/actions/captions.ts
@@ -1,9 +1,44 @@
+import { options } from "yargs";
 import { MulmoStudio, MulmoStudioContext, MulmoCanvasDimension, BeatMediaType } from "../types/index.js";
-import { GraphAILogger } from "graphai";
+import { GraphAI, GraphAILogger, GraphData } from "graphai";
+import * as agents from "@graphai/vanilla";
+
+const { default: __, ...vanillaAgents } = agents;
+
+const graph_data: GraphData = {
+  version: 0.5,
+  nodes: {
+    context: {},
+    map: {  
+      agent: "mapAgent",
+      inputs: { rows: ":context.studio.script.beats", context: ":context" },
+      isResult: true,
+      params: {
+        rowKey: "beat",
+        rowValue: ":context.studio.script.beats[beat]",
+      },
+      graph: {
+        "nodes": {
+          "test": {
+            "agent": "copyAgent",
+            "inputs": {
+              "text": ":beat.text"
+            },
+            "console": {
+              "before": true
+            }
+          }
+        }
+      }
+    },
+  },
+};
 
 export const captions = async (context: MulmoStudioContext) => {
-  const { caption, studio, fileDirs } = context;
-  const { outDirPath } = fileDirs;
+  const { caption } = context;
   GraphAILogger.info("*** DEBUG: captions:", caption);
+  const graph = new GraphAI(graph_data, { ...vanillaAgents });
+  graph.injectValue("context", context);
+  await graph.run();
 };
   
