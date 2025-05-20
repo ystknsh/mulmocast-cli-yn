09f8c6d0e77b5a6947f437795445b85a58c7b7ad
snakajima <satoshi.nakajima@gmail.com>
2025-05-20 05:03:26 -0700

    use ffmpeg_context

--- src/agents/add_bgm_agent.ts
+++ src/agents/add_bgm_agent.ts
@@ -3,6 +3,13 @@ import type { AgentFunction, AgentFunctionInfo } from "graphai";
 import ffmpeg from "fluent-ffmpeg";
 import { MulmoScript } from "../types/index.js";
 import { MulmoScriptMethods } from "../methods/index.js";
+import {
+  FfmpegContextAddInput,
+  FfmpegContextInit,
+  FfmpegContextPushFormattedAudio,
+  FfmpegContextGenerateOutput,
+  ffmPegGetMediaDuration,
+} from "../utils/ffmpeg_utils.js";
 
 const addBGMAgent: AgentFunction<{ musicFile: string }, string, { voiceFile: string; outputFile: string; script: MulmoScript }> = async ({
   namedInputs,
@@ -11,45 +18,20 @@ const addBGMAgent: AgentFunction<{ musicFile: string }, string, { voiceFile: str
   const { voiceFile, outputFile, script } = namedInputs;
   const { musicFile } = params;
 
-  const promise = new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(voiceFile, (err, metadata) => {
-      if (err) {
-        GraphAILogger.info("Error getting metadata: " + err.message);
-        reject(err);
-      }
+  const speechDuration = await ffmPegGetMediaDuration(voiceFile);
+  const padding = MulmoScriptMethods.getPadding(script);
+  const totalDuration = (padding * 2) / 1000 + Math.round(speechDuration ?? 0);
+  GraphAILogger.log("totalDucation:", speechDuration, totalDuration);
 
-      const speechDuration = metadata.format.duration;
-      const padding = MulmoScriptMethods.getPadding(script);
-      const totalDuration = (padding * 2) / 1000 + Math.round(speechDuration ?? 0);
-      GraphAILogger.log("totalDucation:", speechDuration, totalDuration);
-
-      const command = ffmpeg();
-      command
-        .input(musicFile)
-        .input(voiceFile)
-        .complexFilter([
-          // Add a 2-second delay to the speech
-          `[1:a]adelay=${padding}|${padding}, volume=4[a1]`, // 4000ms delay for both left and right channels
-          // Set the background music volume to 0.2
-          `[0:a]volume=0.2[a0]`,
-          // Mix the delayed speech and the background music
-          `[a0][a1]amix=inputs=2:duration=longest:dropout_transition=3[amixed]`,
-          // Trim the output to the length of speech + 8 seconds
-          `[amixed]atrim=start=0:end=${totalDuration}[trimmed]`,
-          // Add fade out effect for the last 4 seconds
-          `[trimmed]afade=t=out:st=${totalDuration - padding / 1000}:d=${padding}`,
-        ])
-        .on("error", (err) => {
-          GraphAILogger.info("Error: " + err.message);
-          reject(err);
-        })
-        .on("end", () => {
-          resolve(0);
-        })
-        .save(outputFile);
-    });
-  });
-  await promise;
+  const ffmpegContext = FfmpegContextInit();
+  const musicInputIndex = FfmpegContextAddInput(ffmpegContext, musicFile);
+  const voiceInputIndex = FfmpegContextAddInput(ffmpegContext, voiceFile);
+  ffmpegContext.filterComplex.push(`[${musicInputIndex}:a]volume=0.2[a0]`);
+  ffmpegContext.filterComplex.push(`[${voiceInputIndex}:a]adelay=${padding}|${padding}, volume=4[a1]`);
+  ffmpegContext.filterComplex.push(`[a0][a1]amix=inputs=2:duration=longest:dropout_transition=3[amixed]`);
+  ffmpegContext.filterComplex.push(`[amixed]atrim=start=0:end=${totalDuration}[trimmed]`);
+  ffmpegContext.filterComplex.push(`[trimmed]afade=t=out:st=${totalDuration - padding / 1000}:d=${padding}[aout]`);
+  await FfmpegContextGenerateOutput(ffmpegContext, outputFile, ["-map", "[aout]"]);
 
   return outputFile;
 };
