40c8fa8b119191b576a2dba87be3d29b510e7b02
kawamataryo <ba068082@gmail.com>
2025-05-21 08:49:41 +0900

    Add pdf cmd

---
+++ src/cli/commands/pdf/handler.ts
@@ -0,0 +1,9 @@
+import { images, pdf } from "../../../actions/index.js";
+import { CliArgs } from "@/src/types/cli_types.js";
+import { initializeContext } from "@/src/cli/helpers.js";
+
+export const handler = async (argv: CliArgs<{ i?: string; pdf_mode: string; pdf_size: string }>) => {
+  const context = await initializeContext(argv);
+  await images(context);
+  await pdf(context, argv.pdf_mode, argv.pdf_size);
+};
---
+++ src/cli/commands/pdf/index.ts
@@ -0,0 +1,4 @@
+export const command = "pdf <file>";
+export const desc = "Generate PDF files";
+export { builder } from "./builder.js";
+export { handler } from "./handler.js";
