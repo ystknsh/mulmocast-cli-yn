e95e6a6505168b80f5b44d83b25b84d7a9844b95
kawamataryo <ba068082@gmail.com>
2025-05-21 08:50:57 +0900

    Fix test

--- test/actions/test_images.ts
+++ test/actions/test_images.ts
@@ -1,7 +1,7 @@
 import test from "node:test";
 // import assert from "node:assert";
 
-import { getFileObject } from "../../src/cli/cli.js";
+import { getFileObject } from "../../src/cli/helpers.js";
 import { createOrUpdateStudioData } from "../../src/utils/preprocess.js";
 import { images } from "../../src/actions/images.js";
 
