b89a1a85149cae77222d2a68c93e2c22b5bc2b57
snakajima <satoshi.nakajima@gmail.com>
2025-05-20 20:57:18 -0700

    call google voice API

--- src/agents/tts_google_agent.ts
+++ src/agents/tts_google_agent.ts
@@ -1,13 +1,34 @@
 import { GraphAILogger } from "graphai";
 import type { AgentFunction, AgentFunctionInfo } from "graphai";
+import * as textToSpeech from '@google-cloud/text-to-speech';
+
+const client = new textToSpeech.TextToSpeechClient();
 
 export const ttsGoogleAgent: AgentFunction = async ({ namedInputs, params }) => {
   const { text } = namedInputs;
   const { apiKey, model, voice, suppressError, instructions } = params;
 
-  try {
-    console.info("*** DEBUG ***: ttsGoogleAgent", text);
-    return { buffer: Buffer.from("Hello, world!") };
+  console.info("*** DEBUG ***: ttsGoogleAgent", text);
+
+  // Construct the voice request
+  const voiceParams: textToSpeech.protos.google.cloud.texttospeech.v1.IVoiceSelectionParams = {
+    languageCode: "en-US",
+    ssmlGender: "FEMALE",
+  };
+
+// Construct the request
+const request: textToSpeech.protos.google.cloud.texttospeech.v1.ISynthesizeSpeechRequest = {
+  input: { text: text },
+  voice: voiceParams,
+  audioConfig: { 
+    audioEncoding: 'MP3',
+    speakingRate: 1.0
+  },
+};
+try {
+    // Call the Text-to-Speech API
+    const [response] = await client.synthesizeSpeech(request);
+    return { buffer: response.audioContent };
   } catch (e) {
     if (suppressError) {
       return {
