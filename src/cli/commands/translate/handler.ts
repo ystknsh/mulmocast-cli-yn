551b11ef79a98beef875b0c23bf963b9328f9182
kawamataryo <ba068082@gmail.com>
2025-05-21 10:00:45 +0900

    Remove unnecessary log

--- src/cli/commands/translate/handler.ts
+++ src/cli/commands/translate/handler.ts
@@ -1,10 +1,8 @@
-import { GraphAILogger } from "graphai";
 import { translate } from "../../../actions/index.js";
 import { initializeContext } from "@/src/cli/helpers.js";
 import { CliArgs } from "@/src/types/cli_types.js";
 
 export const handler = async (argv: CliArgs<{ i?: string }>) => {
-  GraphAILogger.log("Running translate command", argv);
   const context = await initializeContext(argv);
   await translate(context);
 };
