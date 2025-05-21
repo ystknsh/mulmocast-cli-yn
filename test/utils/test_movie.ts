8adcb5d405fbba5ef9a70bcd0e3ad6b34ffedbed
snakajima <satoshi.nakajima@gmail.com>
2025-05-21 16:05:51 -0700

    fix ci_test

--- test/utils/test_movie.ts
+++ test/utils/test_movie.ts
@@ -5,12 +5,18 @@ import { getVideoPart, getAudioPart } from "../../src/actions/movie.js";
 
 test("test getVideoParts image", async () => {
   const { videoPart } = getVideoPart(1, "image", 200, { width: 100, height: 300 });
-  assert.equal(videoPart, "[1:v]loop=loop=-1:size=1:start=0,trim=duration=200,fps=30,setpts=PTS-STARTPTS,scale=100:300,setsar=1,format=yuv420p[v1]");
+  assert.equal(
+    videoPart,
+    "[1:v]loop=loop=-1:size=1:start=0,trim=duration=200,fps=30,setpts=PTS-STARTPTS,scale=w=100:h=300:force_original_aspect_ratio=decrease,pad=100:300:(ow-iw)/2:(oh-ih)/2:color=black,setsar=1,format=yuv420p[v1]",
+  );
 });
 
 test("test getVideoParts movie", async () => {
   const { videoPart } = getVideoPart(1, "movie", 200, { width: 100, height: 300 });
-  assert.equal(videoPart, "[1:v]trim=duration=200,fps=30,setpts=PTS-STARTPTS,scale=100:300,setsar=1,format=yuv420p[v1]");
+  assert.equal(
+    videoPart,
+    "[1:v]trim=duration=200,fps=30,setpts=PTS-STARTPTS,scale=w=100:h=300:force_original_aspect_ratio=decrease,pad=100:300:(ow-iw)/2:(oh-ih)/2:color=black,setsar=1,format=yuv420p[v1]",
+  );
 });
 
 test("test getAudioPart movie", async () => {
