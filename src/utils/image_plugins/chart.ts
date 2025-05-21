16c7275192f7fdbabb13a0efc34fe89ab0977912
snakajima <satoshi.nakajima@gmail.com>
2025-05-21 15:10:21 -0700

    isPieChart

--- src/utils/image_plugins/chart.ts
+++ src/utils/image_plugins/chart.ts
@@ -8,11 +8,17 @@ const processChart = async (params: ImageProcessorParams) => {
   const { beat, imagePath, canvasSize, textSlideStyle } = params;
   if (!beat.image || beat.image.type !== imageType) return;
 
+  const isCircular =
+    beat.image.chartData.type === "pie" ||
+    beat.image.chartData.type === "doughnut" ||
+    beat.image.chartData.type === "polarArea" ||
+    beat.image.chartData.type === "radar";
+  const chart_width = isCircular ? Math.min(canvasSize.width, canvasSize.height) * 0.75 : canvasSize.width * 0.75;
   const template = getHTMLFile("chart");
   const htmlData = interpolate(template, {
     title: beat.image.title,
     style: textSlideStyle,
-    width: Math.round(canvasSize.width * 0.625).toString(),
+    chart_width: chart_width.toString(),
     chart_data: JSON.stringify(beat.image.chartData),
   });
   await renderHTMLToImage(htmlData, imagePath, canvasSize.width, canvasSize.height);
