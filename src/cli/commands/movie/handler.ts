7ce218be0981c328c7168c5c8770694c0b2c6653
kawamataryo <ba068082@gmail.com>
2025-05-21 09:54:41 +0900

    Move audio images

--- src/cli/commands/movie/handler.ts
+++ src/cli/commands/movie/handler.ts
@@ -4,10 +4,10 @@ import { initializeContext } from "@/src/cli/helpers.js";
 
 export const handler = async (argv: CliArgs<{ a?: string; i?: string; c?: string }>) => {
   const context = await initializeContext(argv);
+  await audio(context);
+  await images(context);
   if (context.caption) {
     await captions(context);
   }
-  await audio(context);
-  await images(context);
   await movie(context);
 };
