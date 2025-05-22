f6d0dd5306f07ff8dc3b9f3ce1bb69dc9e757015
snakajima <satoshi.nakajima@gmail.com>
2025-05-22 09:20:42 -0700

    setSessionState

---
+++ src/methods/mulmo_studio.ts
@@ -0,0 +1,15 @@
+import { MulmoStudio } from "../types/index.js";
+import { GraphAILogger } from "graphai";
+
+type ChangeType = "generatingAudio" | "generatingImage" | "generatingVideo" | "generatingMultiLingual";
+
+const notifyStateChange = (studio: MulmoStudio, changeType: ChangeType) => {
+  GraphAILogger.info(`*** ${changeType} changed to ${studio.state[changeType]}`);
+};
+
+export const MulmoStudioMethods = {
+  setSessionState(studio: MulmoStudio, changeType: ChangeType, value: boolean) {
+    studio.state[changeType] = value;
+    notifyStateChange(studio, changeType);
+  },
+};
