3a8d43b13ab55d9c6db403ba3b5802c50d6c2576
snakajima <satoshi.nakajima@gmail.com>
2025-05-27 16:49:06 +0900

    paper to reference type

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -269,7 +269,7 @@ export const mulmoReferenceSchema = z.object({
   url: URLStringSchema,
   title: z.string().optional(),
   description: z.string().optional(),
-  type: z.union([z.literal("article"), z.literal("image"), z.literal("video"), z.literal("audio")]).default("article"),
+  type: z.union([z.literal("article"), z.literal("paper"), z.literal("image"), z.literal("video"), z.literal("audio")]).default("article"),
 });
 
 export const mulmoScriptSchema = mulmoPresentationStyleSchema
