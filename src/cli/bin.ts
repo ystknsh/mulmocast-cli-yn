30aac13821d4215071b9ba55f393a3026d81cce1
ystknsh <yasutaka.nishii@gmail.com>
2025-05-30 15:56:55 +0900

    insert .version()

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -14,6 +14,7 @@ import { GraphAILogger } from "graphai";
 export const main = async () => {
   const cli = yargs(hideBin(process.argv))
     .scriptName("mulmo")
+    .version()
     .usage("$0 <command> [options]")
     .option("v", {
       alias: "verbose",
