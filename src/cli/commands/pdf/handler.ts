b75b66da0eeb270a44f284525933d944595b102b
kawamataryo <ryo.kawamata0425@gmail.com>
2025-06-01 21:28:12 +0900

    wip

--- src/cli/commands/pdf/handler.ts
+++ src/cli/commands/pdf/handler.ts
@@ -1,10 +1,16 @@
-import { images, pdf } from "../../../actions/index.js";
+import { images, pdf, pdfPuppeteer } from "../../../actions/index.js";
 import { CliArgs } from "../../../types/cli_types.js";
 import { initializeContext, runTranslateIfNeeded } from "../../../cli/helpers.js";
 
-export const handler = async (argv: CliArgs<{ i?: string; pdf_mode: string; pdf_size: string }>) => {
+export const handler = async (argv: CliArgs<{ i?: string; pdf_mode: string; pdf_size: string; pdf_engine: string }>) => {
   const context = await initializeContext(argv);
   await runTranslateIfNeeded(context, argv);
   await images(context);
-  await pdf(context, argv.pdf_mode, argv.pdf_size);
+
+  // Choose PDF generation engine based on pdf_engine option
+  if (argv.pdf_engine === "puppeteer") {
+    await pdfPuppeteer(context, argv.pdf_mode, argv.pdf_size);
+  } else {
+    await pdf(context, argv.pdf_mode, argv.pdf_size);
+  }
 };
