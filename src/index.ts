a31a566c25fc358675193ebf432a790078cdbe98
isamu <isamu@to-kyo.to>
2025-06-06 04:55:40 +0900

    export files as library.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./actions/index.js";
+export * from "./cli/helpers.js";
+export * from "./utils/file.js";
+
+export * from "./types/index.js";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./type.js";
 export * from "./schema.js";
+export * from "./schema.js";
