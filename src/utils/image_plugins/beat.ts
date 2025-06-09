1e01fd2020b2d7c0538a385b913649ffbf2f207c
snakajima <satoshi.nakajima@gmail.com>
2025-06-09 10:52:49 -0700

    fix eslint

--- src/utils/image_plugins/beat.ts
+++ src/utils/image_plugins/beat.ts
@@ -2,7 +2,7 @@ import { ImageProcessorParams } from "../../types/index.js";
 
 export const imageType = "beat";
 
-export const processBeatReference = async (params: ImageProcessorParams) => {
+export const processBeatReference = async (__: ImageProcessorParams) => {
   // For beat reference, return undefined to indicate no image should be generated
   // The actual reference will be resolved in mergeResult
   return undefined;
