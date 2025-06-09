67b6a523f46ff2cdc03f8df6c2c453cfa98c050b
snakajima <satoshi.nakajima@gmail.com>
2025-06-09 13:29:28 -0700

    clean up as well

--- src/agents/tts_google_agent.ts
+++ src/agents/tts_google_agent.ts
@@ -50,7 +50,7 @@ const ttsGoogleAgentInfo: AgentFunctionInfo = {
   description: "Google TTS agent",
   category: ["tts"],
   author: "Receptron Team",
-  repository: "https://github.com/receptron/graphai-agents/tree/main/tts/tts-openai-agent",
+  repository: "https://github.com/receptron/mulmocast-cli/",
   license: "MIT",
   environmentVariables: ["OPENAI_API_KEY"],
 };
