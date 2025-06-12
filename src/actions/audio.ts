4eb9b1a19f550472275f753cccef4a6bbbaad599
snakajima <satoshi.nakajima@gmail.com>
2025-06-12 14:50:43 -0700

    respect movieDuration when text property was empty

--- src/actions/audio.ts
+++ src/actions/audio.ts
@@ -47,7 +47,7 @@ const getAudioPath = (context: MulmoStudioContext, beat: MulmoBeat, audioFile: s
     }
     throw new Error("Invalid audio source");
   }
-  if (beat.text === "") {
+  if (beat.text === undefined || beat.text === "") {
     return undefined; // It indicates that the audio is not needed.
   }
   return getAudioSegmentFilePath(audioDirPath, context.studio.filename, audioFile);
