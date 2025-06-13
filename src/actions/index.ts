49c71dc9be9a88c8b1bdf19016b466c4e736c63b
kawamataryo <ba068082@gmail.com>
2025-06-13 14:43:22 +0900

    Rename

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,6 @@
 export * from "./audio.js";
 export * from "./images.js";
 export * from "./movie.js";
-export * from "./pdf_puppeteer.js";
+export * from "./pdf.js";
 export * from "./translate.js";
 export * from "./captions.js";
