c86ba5e658f3c0663763edc160ff18552fb659ab
isamu <isamu@to-kyo.to>
2025-06-15 08:22:49 +0900

    JSON.stringify

--- test/fake/sample.ts
+++ test/fake/sample.ts
@@ -2,4 +2,4 @@ import { generateMock } from "@anatine/zod-mock";
 import { mulmoScriptSchema } from "../../src/types/schema.js";
 
 const mockData = generateMock(mulmoScriptSchema);
-console.log(mockData);
+console.log(JSON.stringify(mockData, null, 2));
