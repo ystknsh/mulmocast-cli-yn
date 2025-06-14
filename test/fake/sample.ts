a45761891c3ecda0cde56d259687ac6d22eaa764
isamu <isamu@to-kyo.to>
2025-06-15 08:12:42 +0900

    add fake data generator

---
+++ test/fake/sample.ts
@@ -0,0 +1,5 @@
+import { generateMock } from '@anatine/zod-mock';
+import { mulmoScriptSchema } from "../../src/types/schema.js";
+
+const mockData = generateMock(mulmoScriptSchema);
+console.log(mockData);
