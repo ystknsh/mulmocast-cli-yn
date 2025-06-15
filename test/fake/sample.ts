2556e51f98573644b9828c1219fb73ca2e0f1371
isamu <isamu@to-kyo.to>
2025-06-15 09:36:42 +0900

    fix lint error

--- test/fake/sample.ts
+++ test/fake/sample.ts
@@ -1,5 +1,6 @@
+import { GraphAILogger } from "graphai";
 import { generateMock } from "@anatine/zod-mock";
 import { mulmoScriptSchema } from "../../src/types/schema.js";
 
 const mockData = generateMock(mulmoScriptSchema);
-console.log(JSON.stringify(mockData, null, 2));
+GraphAILogger.info(JSON.stringify(mockData, null, 2));
