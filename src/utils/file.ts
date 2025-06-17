7d9d4c65b52fb4fc83b8509300674d77b7f10b7e
snakajima <satoshi.nakajima@gmail.com>
2025-06-17 16:12:27 -0700

    reverted

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -84,6 +84,9 @@ export const fetchMulmoScriptFile = async (url: string): Promise<{ result: boole
   }
 };
 
+export const getOutputStudioFilePath = (outDirPath: string, fileName: string) => {
+  return path.resolve(outDirPath, fileName + "_studio.json");
+};
 export const getOutputMultilingualFilePath = (outDirPath: string, fileName: string) => {
   return path.resolve(outDirPath, fileName + "_lang.json");
 };
