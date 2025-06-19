330a6245e2c0186ad4881f1278524ad0f262d88d
isamu <isamu@to-kyo.to>
2025-06-20 06:11:14 +0900

    add test

--- test/release_test.sh
+++ test/release_test.sh
@@ -4,3 +4,5 @@ yarn run cli movie scripts/test/test_media.json
 yarn run cli movie scripts/test/test_movie.json
 yarn run cli movie scripts/test/test_beats.json
 
+yarn run cli movie scripts/snakajima/digital_democracy.json
+yarn run cli movie scripts/test/test_spillover.json
