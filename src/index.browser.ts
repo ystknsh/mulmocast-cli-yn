93193275f5094eee53c0dd391284e3daf3f7ece7
kawamataryo <ba068082@gmail.com>
2025-06-21 15:15:14 +0900

    Fix type export

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,5 +1,5 @@
 // Entry point that exposes only APIs available for use in the browser
 
+export * from "./types/type.js";
 export * from "./types/schema.js";
 export * from "./agents/validate_schema_agent.js";
-export * from "./types/type.js";
