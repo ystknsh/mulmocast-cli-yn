2e6aedaf15647f7ed1ed6d1321856486c5cc33db
isamu <isamu@to-kyo.to>
2025-07-12 15:49:33 +0900

    export

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 export * from "./audio.js";
 export * from "./images.js";
 export * from "./image_references.js";
+export * from "./image_agents.js";
 export * from "./movie.js";
 export * from "./pdf.js";
 export * from "./translate.js";
