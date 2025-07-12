b906f2d94ec4113a2764659b6c972b430404bf92
isamu <isamu@to-kyo.to>
2025-07-12 09:54:11 +0900

    add type for image agent

---
+++ src/types/agent.ts
@@ -0,0 +1,26 @@
+// for image agent
+
+// NOTE: gpt-image-1 supports only '1024x1024', '1024x1536', '1536x1024'
+export type OpenAIImageSize = "1792x1024" | "1024x1792" | "1024x1024" | "1536x1024" | "1024x1536";
+export type OpenAIImageModeration = "low" | "auto";
+export type OpenAIImageOptions = {
+  model: string;
+  prompt: string;
+  n: number;
+  size: OpenAIImageSize;
+  moderation?: OpenAIImageModeration;
+};
+
+export type AgentBufferResult = { buffer: Buffer };
+export type AgentPromptInputs = { prompt: string };
+
+export type ImageAgentParams = { model: string; canvasSize: { width: number; height: number } };
+export type OpenAIImageAgentParams = ImageAgentParams & { moderation: OpenAIImageModeration | null | undefined };
+export type OpenAIImageAgentConfig = { baseURL?: string; apiKey?: string };
+export type OpenAIImageAgentInputs = AgentPromptInputs & { referenceImages: string[] | null | undefined };
+
+export type GoogleImageAgentConfig = {
+  projectId?: string;
+  token?: string;
+};
+// end of image agent
