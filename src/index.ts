06fd6418e2fa2c7f946e07ca5561af89aa56deb9
kawamataryo <ba068082@gmail.com>
2025-07-15 05:51:36 +0900

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// NOTE: If you want to support usage in the browser codebase, also add to src/index.browser.ts
+
 export * from "./actions/index.js";
 export * from "./cli/helpers.js";
 export * from "./utils/file.js";
