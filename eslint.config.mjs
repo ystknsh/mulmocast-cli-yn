fb9024f9273bb1fae0965424a51c22f769156957
isamu <isamu@to-kyo.to>
2025-07-15 07:01:45 +0900

    set cognitive-complexity error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -50,7 +50,6 @@ export default [
       "sonarjs/redundant-type-aliases": "off",
       "sonarjs/todo-tag": "off",
       "sonarjs/no-commented-code": "off",
-      "sonarjs/cognitive-complexity": "warn",
       "sonarjs/no-unused-vars": "off",
     },
     plugins: {
