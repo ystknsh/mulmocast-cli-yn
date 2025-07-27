992dd22668289e11a56604395ea96614b8a1c26a
isamu <isamu@to-kyo.to>
2025-07-28 06:32:58 +0900

    delete mulmo_script_template methods

--- src/methods/mulmo_script_template.ts
+++
@@ -1,8 +0,0 @@
-import { MulmoScript, MulmoPromptTemplate } from "../types/index.js";
-import { getMulmoScriptTemplateSystemPrompt } from "../utils/prompt.js";
-
-export const MulmoScriptTemplateMethods = {
-  getSystemPrompt(template: MulmoPromptTemplate, script?: MulmoScript): string {
-    return getMulmoScriptTemplateSystemPrompt(template, script);
-  },
-};
