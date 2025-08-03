baf194162e6cfa701b02117653ceb59adc529432
isamu <isamu@to-kyo.to>
2025-08-03 16:02:07 +0900

    Add language

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -7,7 +7,7 @@ export const cacheDirName = "cache";
 
 export const pdf_modes = ["slide", "talk", "handout"];
 export const pdf_sizes = ["letter", "a4"];
-export const languages = ["en", "ja", "fr", "es"];
+export const languages = ["en", "ja", "fr", "es", "de", "zh-CN", "zh-TW", "ko", "it", "pt", "ar", "hi"];
 
 export const storyToScriptGenerateMode = {
   stepWise: "step_wise",
