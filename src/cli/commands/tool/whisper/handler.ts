9a2c9480cadfd692b5b4837890e70f08712f05be
snakajima <satoshi.nakajima@gmail.com>
2025-08-20 16:48:21 -0700

    tool whisper

---
+++ src/cli/commands/tool/whisper/handler.ts
@@ -0,0 +1,5 @@
+import { ToolCliArgs } from "../../../../types/cli_types.js";
+
+export const handler = async (argv: ToolCliArgs) => {
+  console.log("whisper");
+};
---
+++ src/cli/commands/tool/whisper/index.ts
@@ -0,0 +1,4 @@
+export const command = "whisper";
+export const desc = "Print whisper to console";
+export { builder } from "./builder.js";
+export { handler } from "./handler.js";
