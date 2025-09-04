5cd9c352b15425e2dedc5379b869563a1e89bea9
kawamataryo <ba068082@gmail.com>
2025-09-04 18:18:55 +0900

    Fix build error

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,8 @@
-export * from "./audio.js";
-export * from "./images.js";
+export { audio, generateBeatAudio, getBeatAudioPath, listLocalizedAudioPaths } from "./audio.js";
+export { images, generateBeatImage, graphOption } from "./images.js";
+export { captions } from "./captions.js";
 export * from "./image_references.js";
 export * from "./image_agents.js";
 export * from "./movie.js";
 export * from "./pdf.js";
 export * from "./translate.js";
-export * from "./captions.js";
