d6eafa1a18b5acf543d45d65544919de7c6ea6e8
kawamataryo <ba068082@gmail.com>
2025-09-04 21:47:46 +0900

    Fix export

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,6 @@
-export { audio, generateBeatAudio, getBeatAudioPath, listLocalizedAudioPaths } from "./audio.js";
-export { images, generateBeatImage, graphOption } from "./images.js";
-export { captions } from "./captions.js";
+export * from "./audio.js";
+export * from "./images.js";
+export * from "./captions.js";
 export * from "./image_references.js";
 export * from "./image_agents.js";
 export * from "./movie.js";
